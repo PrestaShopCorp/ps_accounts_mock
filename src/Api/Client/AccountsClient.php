a7a2526b34e6da26d19ddc80850b9c0e3dbd6839
Clément Désiles <1536672+jokesterfr@users.noreply.github.com>
2024-05-23 13:22:26 +0200

    Add an account client mock

---
+++ src/Api/Client/AccountsClient.php
@@ -0,0 +1,44 @@
+<?php
+namespace PrestaShop\Module\PsAccounts\Api\Client;
+
+/**
+ * Class ServicesAccountsClient mock
+ */
+class AccountsClient
+{
+    /**
+     * ServicesAccountsClient constructor.
+     */
+    public function __construct() {
+    }
+
+    /**
+     * Mock the original PS Accounts client
+     * @see https://github.com/PrestaShopCorp/ps_accounts/blob/master/src/Api/Client/AccountsClient.php#L221
+     * @return array response
+     */
+    public function verifyToken()
+    {
+      return [
+        "status" => true,
+        "httpCode" => 200,
+        "body" => null
+      ];
+    }
+
+    /**
+     * Mock the original RefreshShopToken 
+     * @see https://github.com/PrestaShopCorp/ps_accounts/blob/master/src/Api/Client/AccountsClient.php#L106C21-L106C37
+     * @return array response
+     */
+    public function refreshShopToken()
+    {
+      return [
+        "status" => false,
+        "httpCode" => 400,
+        "body"=> [
+          "message" => "Cannot refresh token"
+        ]        
+      ];
+    }
+}
